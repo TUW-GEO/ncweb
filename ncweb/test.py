7ee6badf56f010ecfbb0efe28a879d8cb6d8614e
Markus Pöchtrager <poechtma@gmail.com>
2014-08-28 16:23:34 +0200

    localogc.py -> Start OGCServer text.py -> convert NetCDF to GTiff (using gdal)

--- ncweb/test.py
+++ ncweb/test.py
@@ -5,9 +5,91 @@ import os
 import root_path as root
 import netCDF4
 import glob
+from osgeo import gdal
+import datetime
+import osr
 
 class NetCdfForm(Form):
-    variables = SelectField('test', choices=[])
+    files = SelectField('files', choices=[])
+    variables = SelectField('variables', choices=[])
+    
+def modelout_to_geotif(netcdfile, outdir, VarName):
+    try:
+        nci = gdal.Open('NETCDF:{0}:{1}'.format(netcdfile, VarName))
+        ncd = netCDF4.Dataset(netcdfile, format='NETCDF4')
+    except:
+        print "Could not open input file: {0}".format(netcdfile)
+        return
+
+    try:
+        geotransform = nci.GetGeoTransform()
+    except:
+        print "Could not get geotransform.\n"
+        return
+    try:
+        projection = nci.GetProjection()
+    except:
+        print "Could not get projection.\n"
+        return
+
+    numbands = nci.RasterCount
+    times = [o2d(t) for t in ncd.variables['time']]
+    print "Found {0} bands to process for {1}".format(numbands, VarName)
+    for i in range(numbands):
+        band = nci.GetRasterBand(i + 1)
+        raster = band.ReadAsArray()
+        y, x = raster.shape
+
+        output_file = "{0}_{1}.tif".format(VarName, datetime.datetime.now().strftime('%Y%m%d_%H%M%S'))
+        subdir = os.path.join(outdir, VarName)
+        if not os.path.exists(subdir):
+            os.mkdir(subdir)
+        output_file = os.path.join(subdir, output_file)
+        if os.path.exists(output_file):
+            continue
+        
+        # Create gtif
+        driver = gdal.GetDriverByName("GTiff")
+        
+        dst_ds = driver.Create(output_file, x, y, 1, gdal.GDT_Int32)
+
+        # top left x, w-e pixel resolution, rotation, top left y, rotation, n-s pixel resolution
+        dst_ds.SetGeoTransform(geotransform)
+
+        # set the reference info 
+        dst_ds.SetProjection(projection)
+        
+        print raster
+
+        # write the band
+        outBand = dst_ds.GetRasterBand(1)
+        outBand.WriteArray(raster)
+        
+        dst_ds = None
+
+def readNetCDF(file):
+    try:
+        nc = netCDF4.Dataset(file, format='NETCDF4')
+        
+#         ari_arr = nc.variables['arid regions mask'][:, :]
+#         lat_arr = nc.variables['lat']
+#         lon_arr = nc.variables['lon']
+#         topo_arr = nc.variables['topographic complexity'][:, :]
+#         trop_arr = nc.variables['tropical forest mask'][:, :]
+#         wetland_arr = nc.variables['wetland fraction'][:, :]
+        allVars = ('arid regions mask', 'topographic complexity', 'tropical forest mask', 'wetland fraction')
+        for var in allVars:
+            modelout_to_geotif(file, os.path.join("/home/pydev/GTiff"), 'tropical forest mask')
+        
+        i = 0
+        variablechoices = []
+        for variable in nc.variables:
+            variablechoices.append((str(i), variable.encode('ascii', 'ignore')))
+            i += 1
+        return [file, nc, variablechoices]
+    except:
+        pass
+
 
 app = Flask(__name__)
 
@@ -17,22 +99,21 @@ def index():
     path = os.path.join(root.x, "students", "mpoecht", "*.nc")
 
     netcdf_files = glob.glob(path)
-    variablechoices = []
+    filechoices = []
     netcdfData = []
-    for file1 in netcdf_files:
-        try:
-            data = netCDF4.Dataset(file1, format='NETCDF4')
-            netcdfData.append(data)
-            i = 0
-            for variable in data.variables:
-                variablechoices.append((str(i), variable.encode('ascii', 'ignore')))
-                i += 1
-        except:
-            pass
+    allData = []
+    j = 0
+    for file in netcdf_files:
+        data = readNetCDF(file)
+        allData.append(data)
+        filechoices.append((str(j), data[0]))
+        netcdfData.append(data[1])
+        j += 1
     pass
-    form.variables.choices = variablechoices
+    form.files.choices = filechoices
+    form.variables.choices = allData[0][2]
     # return netcdfData[0].dataset_name
-    return render_template('netcdfData.html', form=form, netcdfData=netcdfData)
+    return render_template('netcdfData.html', form=form, netcdfData=netcdfData, allData=allData)
 
 @app.route('/hello')
 def hello():
---
+++ ncweb/wms_tests/localogc.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from os import path
+from ogcserver.wsgi import WSGIApp
+import ogcserver
+import pkg_resources
+import testGetMap
+
+def startOgcServer(configfile, mapfile):
+    if not configfile:
+        configfile = path.join('default.conf')
+    if not mapfile:
+        mapfile = path.join('map.xml')
+
+    application = WSGIApp(configfile, mapfile)
+    host = '0.0.0.0'
+    port = 8000
+    httpd = make_server(host, port, application)
+    print "Listening at %s:%s...." % (host, port)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    configfile = path.join('default.conf')
+    mapfile = path.join('map.xml')
+    startOgcServer(configfile, mapfile)
